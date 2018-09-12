829230d51568e74cb12045b52fb8a38e69557d4a
Tamim22 <16079566@student.hhs.nl>
2018-09-12 17:06:21 +0200

    test

---
+++ Canvas opdrachten/Pe4_3.py
@@ -0,0 +1,3 @@
+uurloon = input("Wat verdien je per uur?: ")
+aantalUur = input("Hoeveel uur heb je gewerkt?: ")
+print(aantalUur + " " + "uur" + " " + "werken levert", float(aantalUur) * float(uurloon), "euro op")
